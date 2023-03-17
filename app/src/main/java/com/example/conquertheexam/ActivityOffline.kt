be9d843f3503d3cba345bad9374cd462a1d262ea
theanhdz533 <87807585+theanhdz533@users.noreply.github.com>
2023-03-17 22:18:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/conquertheexam/ActivityOffline.kt
@@ -0,0 +1,11 @@
+package com.example.conquertheexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityOffline : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_offline)
+    }
+}
---
+++ app/src/main/java/com/example/conquertheexam/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.conquertheexam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.conquertheexam.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // btn offline
+        binding.btnTuLuyen.setOnClickListener {
+              val intent = Intent(this,ActivityOffline::class.java)
+             startActivity(intent)
+        }
+    }
+}
